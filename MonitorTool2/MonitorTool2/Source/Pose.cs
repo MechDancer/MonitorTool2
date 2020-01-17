2046615b2f8a3b10865c9cdaea69c522bb37feda
Ydr <ydrml@hotmail.com>
2020-01-17 17:16:42 +0800

    实现四元数和三维位姿

---
+++ MonitorTool2/MonitorTool2/Source/Pose.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace MonitorTool2.Source {
+    /// <summary>
+    /// 三维位姿
+    /// </summary>
+    public struct Pose : IEquatable<Pose> {
+        public Vector3 P { get; }
+        public Vector3 D { get; }
+
+        public Pose(Vector3 p, Vector3 d) {
+            P = p;
+            D = d;
+        }
+
+        public Pose AddDelta(Pose delta) {
+            ToQuaernions(out var v0, out var q0);
+            delta.ToQuaernions(out var v1, out var q1);
+            return new Pose(v0 + q0 * v1 * q0.Conjugate, q1 * q0);
+        }
+        public Pose SubtractDelta(Pose delta) {
+            ToQuaernions(out var v0, out var q0);
+            delta.ToQuaernions(out var v1, out var q1);
+            var qi = q1.Conjugate * q0;
+            return new Pose(v0 - qi * v1 * qi.Conjugate, qi);
+        }
+        public Pose SubtractState(Pose mark) {
+            mark.ToQuaernions(out var v0, out var q0);
+            ToQuaernions(out var v1, out var q1);
+            return new Pose(q0.Conjugate * (v1 - v0) * q0, q1 * q0.Conjugate);
+        }
+
+        public static bool operator ==(Pose a, Pose b)
+            => a.Equals(b);
+        public static bool operator !=(Pose a, Pose b)
+            => !a.Equals(b);
+
+        public bool Equals(Pose other)
+            => P == other.P && D == other.D;
+        public override bool Equals(object obj)
+            => obj is Pose pose && Equals(pose);
+        public override int GetHashCode()
+            => P.GetHashCode() ^ D.GetHashCode();
+
+        public override string ToString() {
+            var half = D.Length();
+            if (MathF.Abs(half) < float.Epsilon)
+                return $"P = {View(P)}, D = {View(default)}, θ = 0 rad";
+            else
+                return $"P = {View(P)}, D = {View(D / half)}, θ = {2 * half} rad";
+        }
+
+        private void ToQuaernions(out Quaternion p, out Quaternion d) {
+            p = new Quaternion(0, P);
+            var half = D.Length();
+            d = new Quaternion(MathF.Cos(half),
+                               MathF.Abs(half) < float.Epsilon
+                               ? default
+                               : D / half * MathF.Sin(half));
+        }
+
+        private Pose(Quaternion p, Quaternion d) {
+            Debug.Assert(MathF.Abs(p.R) < float.Epsilon);
+            P = p.V;
+            var half = d.V.Length();
+            D = MathF.Abs(half) < float.Epsilon ? default : d.V / half * MathF.Atan2(half, d.R);
+        }
+
+        private static string View(Vector3 v) =>
+            $"({v.X}, {v.Y}, {v.Z})";
+    }
+}
---
+++ MonitorTool2/MonitorTool2/Source/Quaternion.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Numerics;
+
+namespace MonitorTool2.Source {
+    /// <summary>
+    /// 四元数
+    /// </summary>
+    public struct Quaternion : IEquatable<Quaternion> {
+        private readonly Vector4 _value;
+
+        public Quaternion(Vector4 value) {
+            _value = value;
+        }
+        public Quaternion(float a, float b, float c, float d)
+            : this(new Vector4(a, b, c, d)) { }
+        public Quaternion(float r, Vector3 v)
+            : this(new Vector4(r, v.X, v.Y, v.Z)) { }
+
+        public float R => _value.X;
+        public Vector3 V => new Vector3(_value.Y, _value.Z, _value.W);
+        public float Square => _value.LengthSquared();
+        public float Length => _value.Length();
+        public Quaternion Conjugate => new Quaternion(R, -V);
+        public Quaternion Inverse => new Quaternion();
+
+        public Quaternion Negate() => new Quaternion(-_value);
+        public Quaternion Add(Quaternion others) => new Quaternion(_value + others._value);
+        public Quaternion Subtract(Quaternion others) => new Quaternion(_value - others._value);
+        public Quaternion Multiply(float k) => new Quaternion(_value * k);
+        public Quaternion Divide(float k) => new Quaternion(_value / k);
+        public Quaternion Multiply(Quaternion others) {
+            var (a, b, c, d) = this;
+            var (e, f, g, h) = others;
+            return new Quaternion(
+                a * e - b * f - c * g - d * h,
+                b * e + a * f - d * g + c * h,
+                c * e + d * f + a * g - b * h,
+                d * e - c * f + b * g + a * h);
+        }
+
+        public void Deconstruct(out float a, out float b, out float c, out float d) {
+            a = _value.X;
+            b = _value.Y;
+            c = _value.Z;
+            d = _value.W;
+        }
+
+        public static Quaternion operator -(Quaternion q)
+            => q.Negate();
+        public static Quaternion operator +(Quaternion a, Quaternion b)
+            => a.Add(b);
+        public static Quaternion operator -(Quaternion a, Quaternion b)
+            => a.Subtract(b);
+        public static Quaternion operator *(Quaternion q, float k)
+            => q.Multiply(k);
+        public static Quaternion operator /(Quaternion q, float k)
+            => q.Divide(k);
+        public static Quaternion operator *(float k, Quaternion q)
+            => q.Multiply(k);
+        public static Quaternion operator /(float k, Quaternion q)
+            => q.Divide(k);
+        public static Quaternion operator *(Quaternion a, Quaternion b)
+            => a.Multiply(b);
+        public static bool operator ==(Quaternion a, Quaternion b)
+            => a.Equals(b);
+        public static bool operator !=(Quaternion a, Quaternion b)
+            => !a.Equals(b);
+
+        public bool Equals(Quaternion other)
+            => _value == other._value;
+        public override bool Equals(object obj)
+            => obj is Quaternion q && Equals(q);
+        public override int GetHashCode()
+            => _value.GetHashCode();
+    }
+}
